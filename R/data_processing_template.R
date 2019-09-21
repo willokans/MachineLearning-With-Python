81d155097346b1296ea2025fa455a0e5ee6af9ac
Will <willokans@outlook.com>
2019-09-21 09:34:53 +0100

    Complete data processing task

---
+++ R/data_processing_template.R
@@ -0,0 +1,23 @@
+#Data Processing
+
+#import dataset
+dataset = read.csv('Data.csv')
+dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
+
+#Encording categorical data
+dataset$Country = factor(dataset$Country, level = c('France', 'Spain', 'Germany'), 
+                         labels = c(1,2,3))
+
+dataset$Purchased = factor(dataset$Purchased, level = c('No', 'Yes'), 
+                         labels = c(0,1))
+
+#splitting the dataset into the training set and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split ==TRUE)
+test_set = subset(dataset, split ==FALSE)
